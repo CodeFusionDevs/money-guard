4ff53ab74d7099223bd0d82e932430db4e299f17
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-02-24 00:43:18 +0300

    Add new routes and page components for login, register, dashboard, and 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import HeaderLayout from "./layout/HeaderLayout";
+import RegisterPage from "./pages/register/RegisterPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
+import NotFoundPage from "./pages/notFound/NotFoundPage";
+
+// Sayfalar eklenecek
 
 function App() {
   return (
     <BrowserRouter>
       <HeaderLayout />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/notFound/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import styles from "./notFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>NotFoundPage</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
