7993ee521217badaeb4db6ed53722f4433916849
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-02-23 16:55:45 +0300

    Set up Redux store with persistence and routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./modern-reset.css";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
