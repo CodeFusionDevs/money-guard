96acf0404e2376bba09c385b93e5606eea6e9dc8
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-02-26 18:50:56 +0300

    Update Redux store import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./modern-reset.css";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import store from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./redux/store.js";
 
