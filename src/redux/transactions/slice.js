62730bdb0f5febd5dc25ea50745c5f73e56cf7f1
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-02-26 17:29:33 +0300

    Add Redux slices and operations for authentication, modal, and transactions

---
+++ src/redux/transactions/slice.js
@@ -0,0 +1,81 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  transactions: [],
+  transactionCategories: [],
+  isLoading: false,
+  error: null,
+};
+
+const transactionsSlice = createSlice({
+  name: "transactions",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTransactions.fulfilled, (state, action) => {
+        state.transactions = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getTransactions.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTransactions.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(getCategories.fulfilled, (state, action) => {
+        state.transactionCategories = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getCategories.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCategories.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(createTransaction.fulfilled, (state, action) => {
+        state.transactions.push(action.payload);
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(createTransaction.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTransaction.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(editTransaction.fulfilled, (state, action) => {
+        state.transactions = state.transactions.map((transaction) =>
+          // Burada bi sıkıntı var sanki. Ama yok da gibi.
+          transaction.id === action.payload.id ? action.payload : transaction
+        );
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(editTransaction.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(editTransaction.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(deleteTransaction.fulfilled, (state, action) => {
+        state.transactions = state.transactions.filter(
+          (transaction) => transaction.id !== action.payload.id
+        );
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(deleteTransaction.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteTransaction.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
