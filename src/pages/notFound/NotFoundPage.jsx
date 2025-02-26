13e26ac7ba27bc3fb6787edb92d6966261e01e73
Samet Alptekin Eroğlu <sametalptekin.eroglu@gmail.com>
2025-02-26 15:52:13 +0300

    NotFoundPage added

--- src/pages/notFound/NotFoundPage.jsx
+++ src/pages/notFound/NotFoundPage.jsx
@@ -1,11 +1,28 @@
-import styles from "./notFoundPage.module.css";
+import FormButton from "../../components/common/FormButton/FormButton";
+import styles from "./NotFoundPage.module.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-const NotFoundPage = () => {
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
   return (
-    <div className={styles.container}>
-      <h1>NotFoundPage</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+      <DotLottieReact
+      src="https://lottie.host/1313594c-db25-44bb-92e1-44614d2174b6/oqVb7lOYG2.lottie"
+      loop
+      autoplay
+    />
+        <p className={styles.error}>
+          The page you're seeking is no longer here.
+        </p>
+      </div>
+
+      <Link to="/login">
+        <FormButton type="button" text={"Home"} variant={"multiColorButtton"} />
+      </Link>
     </div>
   );
 };
 
-export default NotFoundPage;
+export default NotFound;
