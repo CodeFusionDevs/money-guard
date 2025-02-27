986f237501e1a58b01774ae3a3c1b83e82f8fed9
Samet Alptekin Eroğlu <sametalptekin.eroglu@gmail.com>
2025-02-27 21:23:23 +0300

    Transaction Modal Added

--- src/components/buttonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/buttonAddTransactions/ButtonAddTransactions.jsx
@@ -1,10 +1,22 @@
-import styles from "./buttonAddTransactions.module.css";
+import React, { useState } from "react";
+import { IoAddSharp } from "react-icons/io5";
+import styles from "./ButtonAddTransactions.module.css";
+import Modal from "../modalAddTansactions/ModalAddTransactions";
 
 const ButtonAddTransactions = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openAddModal = () => setIsModalOpen(true);
+  const closeAddModal = () => setIsModalOpen(false);
+
   return (
-    <div className={styles.container}>
-      <h1>ButtonAddTransactions</h1>
-    </div>
+    <>
+      <button onClick={openAddModal} type="button" className={styles.btn}>
+        <IoAddSharp className={styles.icon} />
+      </button>
+
+      {/* Modal'ı açmak için isModalOpen durumunu kullanıyoruz */}
+      <Modal isOpen={isModalOpen} onClose={closeAddModal} />
+    </>
   );
 };
 
