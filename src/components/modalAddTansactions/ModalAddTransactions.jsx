efb37e3d308b8dfa5cd068a05fc020e25756509b
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-03-01 15:27:56 +0300

    Refactor React import in ModalAddTransactions component

--- src/components/modalAddTansactions/ModalAddTransactions.jsx
+++ src/components/modalAddTansactions/ModalAddTransactions.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { FiCalendar } from "react-icons/fi";
