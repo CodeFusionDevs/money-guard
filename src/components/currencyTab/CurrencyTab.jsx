23b2a8e17b842ba86796278cb8f7288089d29369
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-03-07 00:10:14 +0300

    Implement CurrencyTab component with responsive design and styling

--- src/components/currencyTab/CurrencyTab.jsx
+++ src/components/currencyTab/CurrencyTab.jsx
@@ -1,9 +1,43 @@
+import { useMediaQuery } from "react-responsive";
 import styles from "./currencyTab.module.css";
 
 const CurrencyTab = () => {
+  const isTablet = useMediaQuery({ query: "(max-width: 1279px)" });
+
   return (
     <div className={styles.container}>
-      <h1>CurrencyTab</h1>
+      <table className={styles.currencyTable}>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>USD</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>EUR</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </table>
+      {!isTablet && (
+        <>
+          <p>Volume</p>
+          <p>Volume</p>
+        </>
+      )}
+
+      <div className={styles.currencyTableGraph}>
+        {/* Vercel sıkıntı çıkarabilir */}
+        <img src="src\components\currencyTab\chart.png" alt="volume" />
+      </div>
     </div>
   );
 };
