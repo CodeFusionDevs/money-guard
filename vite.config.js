0b54d3c5e157e1062005be16e16ad2e662f71de5
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-03-08 00:34:47 +0300

    Hadi artık çalış

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: "/money-guard/",
+    base: "/",
     define: {
       [command === "serve" ? "global" : "_global"]: {},
     },
